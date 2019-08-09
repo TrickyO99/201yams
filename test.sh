59c0be51451fea6d8a2876338b7a37f502a9d965
TRIKI Othmane <33462223+TrickyO99@users.noreply.github.com>
2019-08-10 01:38:52 +0200

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,12 @@
+echo "./$1 -h"
+./$1 -h
+echo "\n./$1 0 0 0 0 0 yams_4"
+./$1 0 0 0 0 0 yams_4
+echo "\n./$1 1 2 3 4 5 four_4"
+./$1 1 2 3 4 5 four_4
+echo "\n./$1 2 2 5 4 6 straight_6"
+./$1 2 2 5 4 6 straight_6
+echo "\n./$1 0 0 0 0 0 full_2_3"
+./$1 0 0 0 0 0 full_2_3
+echo "\n./$1 2 3 2 3 2 full_2_3"
+./$1 2 3 2 3 2 full_2_3
